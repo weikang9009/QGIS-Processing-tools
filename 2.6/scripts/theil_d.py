0648dff94837a46a39389184a74947156028beb9
Serge Rey <sjsrey@gmail.com>
2015-02-13 16:22:22 -0700

    clean up docs and handle pysal version check

--- 2.6/scripts/theil_d.py
+++ 2.6/scripts/theil_d.py
@@ -6,16 +6,12 @@
 import pysal 
 import numpy as np
 import processing 
-from processing.tools.vector import VectorWriter
-from qgis.core import *
-from PyQt4.QtCore import *
-
 
 layer = processing.getObject(input)
-data = processing.values(layer,regime,attribute)
+data = processing.values(layer, regime, attribute)
 y = np.array(data[attribute])
 r = np.array(data[regime])
-theil_d = pysal.TheilDSim(y,r,99)
+theil_d = pysal.TheilDSim(y, r, 99)
 
 print "Theil Interregional Inequality Decomposition"
 print "Attribute: ", attribute
@@ -23,5 +19,7 @@ print "Regime:  ", regime
 print "Global inequality T: ", theil_d.T
 print "Within regions: ", theil_d.wg[0][0]
 print "Between regions: ", theil_d.bg[0][0]
+if int(pysal.version.split(".")[1]) > 9: # 10.0-dev handles 0 share cases for the mean correctly
+    print "Exepected between regions: ", theil_d.bg[1:].mean()
 print "p-value: ",theil_d.bg_pvalue[0]
 
