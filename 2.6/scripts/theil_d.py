86c78a9aa75d4761561ddd11e3056b7e9be7b77a
Serge Rey <sjsrey@gmail.com>
2015-02-13 15:16:42 -0700

    update theil to use processing to access attributes

--- 2.6/scripts/theil_d.py
+++ 2.6/scripts/theil_d.py
@@ -1,5 +1,5 @@
 ##Inequality=group
-##input=vector
+##input=vector 
 ##attribute=field input
 ##regime=field input
 
@@ -12,18 +12,16 @@ from PyQt4.QtCore import *
 
 
 layer = processing.getObject(input)
-provider = layer.dataProvider()
-fields = provider.fields()
-f = pysal.open(pysal.examples.get_path(input.replace('.shp','.dbf')))
-
-y = np.array(f.by_col[str(attribute)])
-r = np.array(f.by_col[str(regime)])
+data = processing.values(layer,regime,attribute)
+y = np.array(data[attribute])
+r = np.array(data[regime])
 theil_d = pysal.TheilDSim(y,r,99)
 
-print "Theil Inequality Decomposition"
+print "Theil Interregional Inequality Decomposition"
 print "Attribute: ", attribute
 print "Regime:  ", regime
 print "Global inequality T: ", theil_d.T
 print "Within regions: ", theil_d.wg[0][0]
 print "Between regions: ", theil_d.bg[0][0]
 print "p-value: ",theil_d.bg_pvalue[0]
+
