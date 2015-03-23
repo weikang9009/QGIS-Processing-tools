7c6531b98fd779a9952008fc6134fa3df506777b
Serge Rey <sjsrey@gmail.com>
2015-03-23 11:25:37 -0700

    working rook and queen

--- 2.6/scripts/weights_builder.py
+++ 2.6/scripts/weights_builder.py
@@ -1,6 +1,6 @@
 ##Spatial statistics=group
 ##input=vector
-##contiguity=string ROOK
+##contiguity=selection ROOK; QUEEN
 ##n_neighbors=output vector
 
 import pysal 
@@ -30,25 +30,63 @@ def layerToW(layer, wType='ROOK'):
     i = 0
     v2p = {}
     iterator = layer.getFeatures()
-    for feat in iterator:
-        geom = feat.geometry()
-        if geom.wkbType() == 6: #multipolygon
-            polys = geom.asMultiPolygon()
-            for poly in polys:
+    if wType == 'QUEEN':
+        for feat in iterator:
+            geom = feat.geometry()
+            if geom.wkbType() == 6: #multipolygon
+                polys = geom.asMultiPolygon()
+                for poly in polys:
+                    for part in poly:
+                        for v in part:
+                            if v not in v2p:
+                                v2p[v] = []
+                            v2p[v].append(i)
+            else: # polygon
+                poly = geom.asPolygon()
                 for part in poly:
                     for v in part:
                         if v not in v2p:
                             v2p[v] = []
                         v2p[v].append(i)
-        else: # polygon
-            poly = geom.asPolygon()
-            for part in poly:
-                for v in part:
-                    if v not in v2p:
-                        v2p[v] = []
-                    v2p[v].append(i)
-        i+=1
-
+            i+=1
+    else: #Rook
+        e2p = {}
+        for feat in iterator:
+            geom = feat.geometry()
+            if geom.wkbType() == 6: #multipolygon
+                polys = geom.asMultiPolygon()
+                for poly in polys:
+                    for part in poly:
+                        lnv = len(part)
+                        for oi in range(lnv-1):
+                            o = part[oi]
+                            d = part[oi+1]
+                            od = (o,d)
+                            do = (d,o)
+                            if od not in e2p:
+                                e2p[od] = []
+                            if do not in e2p:
+                                e2p[do] = []
+                            e2p[od].append(i)
+                            e2p[do].append(i)
+            else: # polygon
+                poly = geom.asPolygon()
+                for part in poly:
+                    for v in part:
+                        lnv = len(part)
+                        for oi in range(lnv-1):
+                            o = part[oi]
+                            d = part[oi+1]
+                            od = (o,d)
+                            do = (d,o)
+                            if od not in e2p:
+                                e2p[od] = []
+                            if do not in e2p:
+                                e2p[do] = []
+                            e2p[od].append(i)
+                            e2p[do].append(i)
+            i+=1
+        v2p = e2p
     n = i
     neighbors = dict([ (i,[]) for i in xrange(n)])
     for v in v2p:
@@ -66,15 +104,16 @@ def layerToW(layer, wType='ROOK'):
 
     return pysal.W(neighbors)
 
+contiguity = ["ROOK", "QUEEN"][contiguity]
 
-
-if contiguity == 'queen':
+if contiguity == 'QUEEN':
     print 'INFO:Queen contiguity'
-    w=pysal.queen_from_shapefile(input)
+    #w=pysal.queen_from_shapefile(input)
+    w = layerToW(layer,'QUEEN')
 else:
     print 'INFO: Rook contiguity'
     #w=pysal.rook_from_shapefile(input)
-    w = layerToW(layer)
+    w = layerToW(layer, 'ROOK')
 
 n_neigh = w.cardinalities.values()
 print n_neigh
